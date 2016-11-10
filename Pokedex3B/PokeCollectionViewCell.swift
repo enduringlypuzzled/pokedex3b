bc220559b079939e57e739bb5e4c9cf6fd77c2be
Steve Mecking <stevo@Steves-MacBook-Air.local>
2016-11-10 09:11:51 -0700

    CollectionView Working

---
+++ Pokedex3B/PokeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PokeCollectionViewCell.swift
+//  Pokedex3B
+//
+//  Created by Steve Mecking on 2016-11-10.
+//  Copyright © 2016 Steve Mecking. All rights reserved.
+//
+
+import UIKit
+
+class PokeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+}
--- Pokedex3B/ViewController.swift
+++ Pokedex3B/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Pokedex3B
+//  TestCollection
 //
 //  Created by Steve Mecking on 2016-11-09.
 //  Copyright © 2016 Steve Mecking. All rights reserved.
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let reuseIdentifier = "cell"
+    var items = ["1","2","3","4","5","6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PokeCollectionViewCell
+        cell.myLabel.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.cyan
+        
+        return cell
+        
+    }
 }
 
