7a3ad590a8a1ef950a73f1056e7efd1eb202e6a9
Steve Mecking <stevo@Steves-MacBook-Air.local>
2016-11-15 10:07:00 -0700

    Added info for desc

--- Pokedex3B/PokemonDetailVC.swift
+++ Pokedex3B/PokemonDetailVC.swift
@@ -11,13 +11,24 @@ import UIKit
 class PokemonDetailVC: UIViewController {
 
     var pokemon:Pokemon!
+    //@IBOutlet weak var nameLabel: UILabel!
+    /*@IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
     
+    @IBOutlet weak var evoLabel: UILabel!
+    @IBOutlet weak var currentevoImage: UIImageView!
+    @IBOutlet weak var nextevoImage: UIImageView!*/
     
-    @IBOutlet weak var nameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameLabel.text = pokemon.name
+      // nameLabel.text = pokemon.name
         
         
     }
